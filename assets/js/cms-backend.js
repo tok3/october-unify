63b87f23b5377827100a0db1d43717d6138644bd
Tobias Koch <tok3@eq3w.de>
2019-08-06 11:42:48 +0200

    krempel rein

---
+++ assets/js/cms-backend.js
@@ -0,0 +1,15 @@
+$(function() {
+
+
+    //$("#pages-master-tabs").addClass("hide");
+
+    setTimeout(function() {
+    $('li[data-id="page-unify-weg"] a').trigger( "click" );
+    },10);
+
+  /*  setTimeout(function() {
+    $('#Form-formPage9fd52c181f4597576c48bd40271fa3db-secondaryTabs').addClass("hide");
+    },20);
+*/
+
+});
